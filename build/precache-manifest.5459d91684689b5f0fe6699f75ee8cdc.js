0b3b69f58c22c94e56fe130dcc10fc4ec662bff4
ANDRIAMIADANARY <henintsoaravaka@gmail.com>
2019-04-20 21:46:18 +0300

    Initial commit

---
+++ build/precache-manifest.5459d91684689b5f0fe6699f75ee8cdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ace0692a316324f33087",
+    "url": "/static/js/main.9fcfb261.chunk.js"
+  },
+  {
+    "revision": "9ec1a562162c0d732c1f",
+    "url": "/static/js/2.c9b6d982.chunk.js"
+  },
+  {
+    "revision": "ace0692a316324f33087",
+    "url": "/static/css/main.e7b55145.chunk.css"
+  },
+  {
+    "revision": "b8f60344d8cc2a19af22e4a909a8c330",
+    "url": "/index.html"
+  }
+];
